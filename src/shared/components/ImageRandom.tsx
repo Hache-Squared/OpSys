026066693d37a2f0c20e2c3f5cb4f3a5f342c6cf
Angel <aghh.oficial@gmail.com>
2024-05-06 11:02:33 -0600

    added new images for hero-images

--- src/shared/components/ImageRandom.tsx
+++ src/shared/components/ImageRandom.tsx
@@ -11,6 +11,31 @@ const images = [
   require("../../assets/hero-images/7.jpg"),
   require("../../assets/hero-images/8.jpg"),
   require("../../assets/hero-images/9.jpg"),
+  require("../../assets/hero-images/10.jpg"),
+  
+  require("../../assets/hero-images/11.jpg"),
+  require("../../assets/hero-images/12.jpg"),
+  require("../../assets/hero-images/13.jpg"),
+  require("../../assets/hero-images/14.jpg"),
+  require("../../assets/hero-images/15.jpg"),
+  require("../../assets/hero-images/16.jpg"),
+  require("../../assets/hero-images/17.jpg"),
+  require("../../assets/hero-images/18.jpg"),
+  require("../../assets/hero-images/19.jpg"),
+  require("../../assets/hero-images/20.jpg"),
+  
+  require("../../assets/hero-images/21.jpg"),
+  require("../../assets/hero-images/22.jpg"),
+  require("../../assets/hero-images/23.jpg"),
+  require("../../assets/hero-images/24.jpg"),
+  require("../../assets/hero-images/25.jpg"),
+  require("../../assets/hero-images/26.jpg"),
+  require("../../assets/hero-images/27.jpg"),
+  require("../../assets/hero-images/28.jpg"),
+  require("../../assets/hero-images/29.jpg"),
+  require("../../assets/hero-images/30.jpg"),
+
+
 ];
 export const ImageRandom = () => {
   const getRandomImage = () => {
